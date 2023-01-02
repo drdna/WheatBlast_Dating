c3de49297b8f0e7edf18d8a71050e744001f0537
Mark Farman <farman@email.uky.edu>
2023-01-02 10:58:49 -0500

    Update SNPdistribution70-15ref.R

--- scripts/SNPdistribution70-15ref.R
+++ scripts/SNPdistribution70-15ref.R
@@ -11,7 +11,7 @@ xlabels <- function(x) { x/1000000 }
 
 #pdf("SNPdistrib.pdf", 8.5, 9.1)
 ggplot(data = df, aes(pos)) + geom_histogram(binwidth = 20000, fill = "blue") + facet_grid(rows = vars(chrs)) +
-  ylim(-20, 500) +  labs(title = "SNP distribution PY0925 v B71 (Burbano/Latorre data)", size = 10) + 
+  ylim(-20, 500) +  labs(title = "SNP distribution in 70-15 reference genome (Burbano/Latorre data)", size = 10) + 
   xlab("Mb") + ylab("SNPs per 20 kb window") + theme(plot.title = element_text(size = 20)) + 
   geom_segment(data = chrLines, aes(x = 0, y = -20, xend = chrLengths, yend = -20)) +
   scale_x_continuous(breaks = seq(from = 0, to = 8400000, by = 1000000), labels=xlabels)
